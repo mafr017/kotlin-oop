d55144607f5d70bfb47c58771b68774d7af7f77b
M Aditya Fathur Rahman <madityafr3@gmail.com>
2020-12-15 21:13:21 +0700

    Membuat object

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Belajar Dasar Kotlin (Fundamental)
 1. Membuat package Class
+2. Membuat object
---
+++ src/main/kotlin/app/object.kt
@@ -0,0 +1,13 @@
+package app
+
+import data.Person
+
+fun main() {
+    val adit = Person()
+    val fisher = Person()
+    val killjoy = Person()
+
+    println(adit)
+    println(fisher)
+    println(killjoy)
+}
